96c44f2126d798167f88120048e78aa79cd20a16
Justin Czolowski <Jczolowski@yahoo.com>
2020-04-18 23:38:07 -0700

    second commit

--- asssets/javascript/game.js
+++ asssets/javascript/game.js
@@ -1,4 +1,40 @@
-var composerOption ["Bach", "Mozart" , "Ravel" , "Faure" , "Chopin" , "Debussy" , "Wagner" , "Liszt", "Bruckner" , "Brahms" , "Schumann" , "Haydn" , "Tchaikovsky"];
-var word = "string";
+var composerOption [ "bach", "mozart" , "ravel" , "faure" , "chopin" , "debussy" , "wagner" , "liszt", "bruckner" , "brahms" , "schumann" , "haydn" , "tchaikovsky"];
 var wins
 var lossess
+
+
+var composers = composerOption[Math.floor(Math.random() * composerOption.length)];
+
+var answerArray = [];
+for (var i = 0; i < composerOption.length; i++) {
+   answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
+
+
+var guess = prompt("Guess a letter , or click Cancel to stop playing");
+if (guess === null) {
+   break;
+} else if (guess.length !==1) {
+   alert("Enter a single letter");
+}  else {
+   for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+         answerArray[j] = guess;
+         remainingLetters--;
+      }
+   }
+}
+
+function checkUserGuess() {
+   var guess = document.getElementById("user-guess")["0"].value;
+   alert (guess);
+
+    
+}
+
+function displayOutput() {
+
+}
+
