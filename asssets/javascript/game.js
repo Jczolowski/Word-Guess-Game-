fea3cc34b03d96fb8dce0936392ec34a24aecfbb
Justin Czolowski <Jczolowski@yahoo.com>
2020-04-20 04:56:24 -0700

    minor tweaks

--- asssets/javascript/game.js
+++ asssets/javascript/game.js
@@ -1,33 +1,41 @@
 var composerOption = [ "bach", "mozart" , "beethoven" , "mahler" , "wagner" , "chopin" , "haydn" , "ravel" , "faure" , "debussy" , "liszt" , "schumann" , "stravinsky"];
-var randoComposer = getRandoComposer();
-let wordStatus = null;
+var images = [ ]
 
 
-//pick a composer 
-function randomComposer() {
-   answer = composerOption[Math.floor(Math.random() * composerOption.length)];
- }
 
- //check player guess
-function checkPlayerGuess() {
-   var guess = document.getElementById(player-guess)["0"].value;
-      if(guess ==randoComposer) {
-         displayoutput("correct");
-      }
-      else(guess !==randoComposer ); {
 
-         displayoutput("wrong!")
-      }
+
+//show random composer image
+function getRandomComposer()  {
+   var rand = Math.floor(Math.random() *)
+
 }
-//show empty name of Composer with "_"
-function guessedWord() {
-   wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
- 
-   document.getElementById('wordHighlight').innerHTML = wordStatus;
- }
- 
- guessedWord();
+//show random composer name with underscores
+function showRandomComposer() {
 
- function reset() {
+}
 
- }
+//take user guess
+function checkUserGuess()  {
+
+}
+//play song if correct
+function playComposerPiece()  {
+
+}
+//Show win/lose message
+function displayOutput() {
+
+}
+
+//reset game
+function reset() {
+
+}
+
+//show wins/losses
+function showScore() {
+
+}
+
+
