b937211b1a944ec833697c36c8e1eab04f5254ed
Justin Czolowski <Jczolowski@yahoo.com>
2020-04-26 17:21:25 -0700

    added functionality

--- asssets/javascript/game.js
+++ asssets/javascript/game.js
@@ -1,49 +1,64 @@
-var composerOption = [ "bach", "mozart" , "beethoven" , "mahler" , "wagner" , "chopin" , "haydn" , "ravel" , "faure" , "debussy" , "liszt" , "schumann" , "stravinsky"];
-var images = ['bach.jpg' , '.mozart,jpg' , 'beethoven.jpg' 'mahler.jpg' , 'wagner.jpg' , 'chopin.jpg' , 'haydn.jpg ' , 'ravel.jpg', 'faure.jpg' , 'debussy,jpg' , 'liszt.jpg', 'schumann.jpg' , 'stravinsky.jpg']
-
-let answer = '';
-let guessed = [];
-let wordStatus = null;
-let chosenImage
-let chosenComposer
+var composerOption = [ "mozart", "bach" , "beethoven" , "mahler" , "wagner" , "debusy" , "stravinsky" , "faure" , "ravel" , "schumann" , "haydn", "liszt"];
+var images = ['mozart.jpg' , 'bach.jpg' , 'beethoven.jpg' , 'mahler.jpg' , 'wagner.jpg' , 'debussy.jpg' , 'stravinsky.jpg' , 'faure.jpg' , 'ravel.jpg' , 'schumann.jpg' , 'haydn.jpg' , 'liszt.jpg'];
+let chosenImage;
+let chosenComposer;
+var startingImage = document.getElementById("content")
+var button = document.getElementById("new-game");
 
 
 
+//start game
+button.addEventListener("click" , function(){
+   startGame()
+});
+
+
+//pick random composer image and name 
+function getRandomComposer()  {
+   var randNum = Math.floor(Math.random() * composerOption.length);
+   chosenComposer = composerOption[randNum];
+   chosenImage = images[randNum];
+   console.log(chosenComposer);
+   console.log(chosenImage);
+}
 
 
 //show random composer image
-function getRandomImage()  {
-   chosenImage = images[Math.floor(Math.random() * images.length)];
-   
-
-}
-//show random composer name with underscores
-function getRandomComposer()  {
-   chosenComposer = composerOption[Math.floor(Math.random() * composerOption.length)];
+function startGame() {
+   getRandomComposer()
+   startingImage.src="asssets/images/" + chosenImage
+   console.log("startGame")
 }
 
-//take user guess
-function checkUserGuess()  {
-   document.onkeyup
 
-}
-//play song if correct
-function playComposerPiece()  {
+//take in player guess
 
-}
-//Show win/lose message
-function displayOutput() {
+function userGuess() {
+   document.getElementById("Submit-guess").submit()
 
+   if (guess == chosenComposer) {
+   displayOutput("win");{
+     
+   }
 }
 
-//reset game
-function reset() {
+else {
+   displayOutput("lose"); {
+      
+   }
+  }
+ }
 
-}
 
-//show wins/losses
-function showScore() {
+ //check player guess
+function checkUserGuess() {
+   let guess = document.getElementById("user-guess")["0"].value;
+   if (guess === chosenComposer) {
+      alert("you win!")
+   }
 
-}
-
+   else {
+      alert("thats not right!")
+   }
+   }
 
